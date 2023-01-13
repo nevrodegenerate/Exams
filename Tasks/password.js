4f27da0b371390543192bf183447ae21a5f2e3fc
Bogdan Lepkov <122379140+nevrodegenerate@users.noreply.github.com>
2023-01-13 22:50:37 +0200

    Added 'use strict', changed names of some variables and declarations

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,11 +1,12 @@
 // Generate random password
+'use strict'
 
-let GeneratePassword = (alphabet, length) => {
+const GeneratePassword = (alphabet, passLength) => {
   const MAX = alphabet.length;
-  key = '';
-  for (let i = 0; i < length; i++) {
+  password = '';
+  for (let i = 0; i < passlength; i++) {
     Index = Math.floor(Math.random() * MAX);
-    key = key + alphabet[Index];
+    password = password + alphabet[Index];
   }
   return key;
 };
